96c25dda0cefe90c0d0938466a42414813581474
Diego Lima <diego.lima@lais.huol.ufrn.br>
2019-05-16 15:48:58 -0300

    ajuste dos pesos da última camada, começando ajuste dos pesos das camadas escondidas

--- funcoes.py
+++ funcoes.py
@@ -15,4 +15,26 @@ def sigmoid(z):
     result = 1.0 / (1.0 + np.exp(-z))
     return result
 
+def sigmoid_derivative(z):
+    '''
+    Derivative for Sigmoid function
+    z can be an numpy array or scalar
+    '''
+    result = sigmoid(z) * (1 - sigmoid(z))
+    return result
+
     
+def memoize(func):
+    '''
+    Serve para fazer um cache dos resultados de uma função.
+    '''
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
