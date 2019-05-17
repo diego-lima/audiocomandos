6a4e08bfb873a1a902baee2ca09e28a41191e324
Diego Lima <ddddiegolima@gmail.com>
2019-05-16 21:19:04 -0300

    adicionando comentário e melhorando um pouco a generalização

--- funcoes.py
+++ funcoes.py
@@ -23,6 +23,28 @@ def sigmoid_derivative(z):
     result = sigmoid(z) * (1 - sigmoid(z))
     return result
 
+def relu(z):
+    '''
+    Rectified Linear function
+    z can be an numpy array or scalar
+    '''
+    if np.isscalar(z):
+        result = np.max((z, 0))
+    else:
+        zero_aux = np.zeros(z.shape)
+        meta_z = np.stack((z , zero_aux), axis = -1)
+        result = np.max(meta_z, axis = -1)
+    return result
+    
+
+def relu_derivative(z):
+    '''
+    Derivative for Rectified Linear function
+    z can be an numpy array or scalar
+    '''
+    result = 1 * (z > 0)
+    return result
+
     
 def memoize(func):
     '''
