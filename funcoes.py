0a2df7e88f866724ed4b9d3016567907b67faafc
Diego Lima <ddddiegolima@gmail.com>
2019-05-18 23:07:44 -0300

    Fazendo data engineering: pipeline para extração de features

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,7 @@
 from typing import Union
 import numpy as np
 
-def centro_gravidade(indices: np.array, valores: np.array):
+def calcular_centro_gravidade(indices: np.array, valores: np.array):
     """Pega uma série numérica e retorna seu centro de gravidade"""
 
     return (indices*valores).sum() / valores.sum()
